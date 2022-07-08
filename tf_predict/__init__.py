de396451683373e3a5581be96cb669faffc19065
Paul Nderitu <p.nderitu@doctors.org.uk>
2022-07-08 11:56:27 +0100

    License Added

--- tf_predict/__init__.py
+++ tf_predict/__init__.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022, Paul Nderitu
+# All rights reserved.
+# This source code is licensed under the Apache 2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Tensorflow model predictors/tester entry point
-# Paul Nderitu and Diabetes UK 2021
 
 from .curation_prediction import *
